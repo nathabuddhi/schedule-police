82f499ce61606f08556db7c4edfa03a341f9bab3
Natha Buddhi <nathabuddhi@gmail.com>
2025-11-11 09:12:16 +0700

    Add NeonDB Connection

---
+++ src/lib/neon.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+
+async function getData() {
+    const sql = neon(process.env.DATABASE_URL ?? "");
+    const response = await sql`SELECT version()`;
+    return response[0].version;
+}
+
+export default async function Page() {
+    const data = await getData();
+    console.log(data);
+}
