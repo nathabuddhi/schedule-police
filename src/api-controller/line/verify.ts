6a6a796f23d8763a8578aeace31bf251c2b0efa4
Natha Buddhi <nathabuddhi@gmail.com>
2025-11-18 14:51:36 +0700

    Add Line Webhook Verification

---
+++ src/api-controller/line/verify.ts
@@ -0,0 +1,32 @@
+import { StandardResponse } from "@/lib/types";
+import crypto from "crypto";
+
+export function verifyLineSignature(
+    payload: string,
+    signature: string
+): StandardResponse<boolean> {
+    try {
+        const secret = process.env.LINE_CHANNEL_SECRET;
+        if (!secret || secret === "")
+            throw new Error("LINE_CHANNEL_SECRET is not defined");
+
+        const hmac = crypto
+            .createHmac("sha256", secret)
+            .update(payload, "utf8")
+            .digest("base64");
+        console.log("Computed HMAC:", hmac);
+        console.log("Received Signature:", signature);
+        return {
+            success: true,
+            message: "Signature verification completed.",
+            data: hmac === signature,
+        };
+    } catch (error) {
+        console.error("Error during signature verification:", error);
+        return {
+            success: false,
+            message: "An error occurred during signature verification.",
+            data: false,
+        };
+    }
+}
---
+++ src/app/api/notify/message/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from "next/server";
+import { successResponse, errorResponse } from "@/lib/types";
+import { verifyLineSignature } from "@/api-controller/line/verify";
+
+export async function POST(request: NextRequest) {
+    const body = await request.text();
+    const signature = request.headers.get("x-line-signature");
+
+    const valid = verifyLineSignature(body, signature || "");
+    if (!valid.success || !valid.data) {
+        return errorResponse("Invalid signature", 401);
+    }
+    return successResponse("Message received", null);
+}
