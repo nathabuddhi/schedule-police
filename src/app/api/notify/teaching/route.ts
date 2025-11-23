a68984a1b129f535fe91a8e966e9b8331958ae42
Natha Buddhi <nathabuddhi@gmail.com>
2025-11-24 02:30:03 +0700

    Change X_AUTH_TOKEN to X_AUTH_SECRET for MainApp.

--- src/app/api/notify/teaching/route.ts
+++ src/app/api/notify/teaching/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
     const token = request.headers.get("X-Auth-Token");
 
-    if (token !== process.env.X_AUTH_TOKEN) {
+    if (token !== process.env.X_AUTH_SECRET) {
         return errorResponse("Unauthorized.", 401);
     }
 
