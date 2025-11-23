bbaccfd299f877366327bcad33a5e2492476494b
Natha Buddhi <nathabuddhi@gmail.com>
2025-11-24 02:19:25 +0700

    Add basic authentication for routes

--- src/app/api/notify/teaching/route.ts
+++ src/app/api/notify/teaching/route.ts
@@ -1,5 +1,13 @@
 import { checkTeachingSchedule } from "@/app/api/api-controller/notify/teaching";
+import { errorResponse } from "@/lib/types";
+import { NextRequest } from "next/server";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
+    const token = request.headers.get("X-Auth-Token");
+
+    if (token !== process.env.X_AUTH_TOKEN) {
+        return errorResponse("Unauthorized.", 401);
+    }
+
     return await checkTeachingSchedule();
 }
